914b0a3db9656bbdca4d072e9d77aa0b8142189f
loophong <1940155085@qq.com>
2024-01-23 16:22:28 +0800

    test1

--- order/src/main/java/com/gulimall/order/GulimallOrderApplication.java
+++ order/src/main/java/com/gulimall/order/GulimallOrderApplication.java
@@ -3,11 +3,15 @@ package com.gulimall.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+import java.util.Set;
+
 @SpringBootApplication
 public class GulimallOrderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GulimallOrderApplication.class, args);
     }
+    String s1 = "1234";
 
 }
