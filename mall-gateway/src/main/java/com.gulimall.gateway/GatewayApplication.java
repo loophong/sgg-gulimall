6217ab499bf49aa5a43affd7eca6464c3e433b89
loophong <1940155085@qq.com>
2025-03-06 19:43:31 +0800

    feat(gateway): 添加网关服务模块，解决依赖问题

--- mall-gateway/src/main/java/com.gulimall.gateway/GatewayApplication.java
+++ mall-gateway/src/main/java/com.gulimall.gateway/GatewayApplication.java
@@ -4,9 +4,22 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
+/**
+
+ 1、开启网关服务注册发现 @EnableDiscoveryClient
+ 2、配置nacos注册中心地址
+ 3、配置路由规则 application.yml
+ spring:
+ datasource:
+ gateway:
+ routes:
+ - id: test_baidu
+ uri: https://www.baidu.com
+ predicates:
+ - Query=url,baidu
+ */
 @EnableDiscoveryClient
 @MapperScan("com.gulimall.gateway.dao")
 @SpringBootApplication
